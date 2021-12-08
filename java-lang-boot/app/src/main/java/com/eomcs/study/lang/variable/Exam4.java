2fea678bb2aaac81242bf96f93b8bcc1a0c5d7a4
supercode333 <gacomonkey123@gmail.com>
2021-12-08 11:53:10 +0900

    day11

--- java-lang-boot/app/src/main/java/com/eomcs/study/lang/variable/Exam4.java
+++ java-lang-boot/app/src/main/java/com/eomcs/study/lang/variable/Exam4.java
@@ -12,7 +12,7 @@ public class Exam4 {
   //테스트:
   // => /static/lang/literal/variable/exam4/test1.html
   @GetMapping("/test1")
-  public String test1(int v1, int v2, String op) {
+  public String test1(int v1, int v2, String op) {  //서버에서 클라이언트가 보낸 데이터를 이렇게 파라미터를 받은 후에 작업을 수행하고 그 결과를 리턴해준다!
     int result = 0;
     switch (op) {
       case "+": result = v1 + v2; break;
@@ -44,7 +44,7 @@ public class Exam4 {
   //테스트:
   // => /static/lang/literal/variable/exam4/test2.html
   @GetMapping("/test2")
-  public Object test2(int v1, int v2, String op) {     // Object는 string이랑 int 둘다 되는거임
+  public Object test2(int v1, int v2, String op) {     // Object는 string이랑 int 둘다 되는거임. 문자열이랑 숫자열 둘다 받고 싶으면!
     int result = 0;
     switch (op) {
       case "+": result = v1 + v2; break;
@@ -56,6 +56,14 @@ public class Exam4 {
     }
 
     return result;  // 클래스명 치고 . 점 찍으면 그걸 다루는 기능들을 쫙 보여준다! 클래스명.기능명(값) // java.lang.은 패키지임! 이 패키지에 있는 경우만 패키지명 생략 가능함! 원래는 java.lang.Integer.toString(result);
+  }
+
+  //테스트:
+  // => /static/lang/literal/variable/exam4/test3.html
+  @GetMapping("/test3")
+  public Object test3() {  //Object 넣으면 어떤 값이라도 return 가능함!
+    String[] names = {"홍길동", "임꺽정", "유관순", "안중근", "윤봉길"};
+    return names;  // 스프링부트는 이 메서드가 리턴한 배열을 JSON 형식의 문자열로 바꿔 응답한다. 
   }
 }
 
