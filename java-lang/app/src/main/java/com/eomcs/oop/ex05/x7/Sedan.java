b58a5cdee092dfd7020147fe0c8e05c4bdc9cd03
supercode333 <gacomonkey123@gmail.com>
2022-01-14 14:05:39 +0900

    .

---
+++ java-lang/app/src/main/java/com/eomcs/oop/ex05/x7/Sedan.java
@@ -0,0 +1,23 @@
+package com.eomcs.oop.ex05.x7;
+
+public class Sedan extends Car {
+
+  boolean auto;
+  boolean openedSunroof;
+
+  @Override
+  public void run() {
+    System.out.println("부드럽게 달린다!");
+  }
+
+  public void openSunroof() {
+    System.out.println("선루프 오픈!");
+    this.openedSunroof = true;
+  } 
+
+  public void closeSunroof() {
+    System.out.println("선루프 클로즈!");
+    this.openedSunroof = false;
+  }
+
+}
