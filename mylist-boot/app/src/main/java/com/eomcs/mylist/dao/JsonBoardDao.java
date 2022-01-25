6be3d130535613f0576e21e41b15c4c096655417
supercode333 <gacomonkey123@gmail.com>
2022-01-25 15:38:10 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/JsonBoardDao.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/JsonBoardDao.java
@@ -2,7 +2,6 @@ package com.eomcs.mylist.dao;
 
 import java.io.File;
 import com.eomcs.mylist.domain.Board;
-import com.eomcs.util.ArrayList;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 // @Repository
@@ -10,74 +9,34 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 // - 또한 이 객체를 원하는 곳에 자동으로 주입한다.
 //
 //@Repository  
-public class JsonBoardDao implements BoardDao {
+public class JsonBoardDao extends AbstractBoardDao {
 
   String filename = "boards.json";
-  ArrayList boardList = new ArrayList(); // 변수 선언 = 변수를 만들라는 명령!
 
   public JsonBoardDao() {
     try {
       ObjectMapper mapper = new ObjectMapper();
-      boardList.addAll(mapper.readValue(new File(filename), Board[].class)); // 파일을 읽고 Board[] 배열로 리턴해줘
+      boardList.addAll(mapper.readValue(new File(filename), Board[].class));
 
     } catch (Exception e) {
       System.out.println("게시글 데이터 로딩 중 오류 발생!");
     }
   }
 
-  private void save() throws Exception {
+  @Override
+  protected void save() throws Exception {
     ObjectMapper mapper = new ObjectMapper();
-    mapper.writeValue(new File(filename), boardList.toArray()); // 첫 번째 파라미터로 파일을 줌! 알아서 하라고~ 간단하게!
-  }
-
-  @Override
-  public int countAll() {
-    return boardList.size();
-  }
-
-  @Override
-  public Object[] findAll() {
-    return boardList.toArray();
-  }
-
-  @Override
-  public void insert(Board board) throws Exception {
-    boardList.add(board);
-    save();
-  }
-
-  @Override
-  public Board findByNo(int no) {
-    if (no < 0 || no >= boardList.size()) {
-      return null;
-    }
-    return (Board) boardList.get(no);
-  }
-
-  @Override
-  public int update(int no, Board board) throws Exception {
-    if (no < 0 || no >= boardList.size()) {
-      return 0;
-    }
-    boardList.set(no, board);
-    save();
-    return 1;
-  }
-
-  @Override
-  public int delete(int no) throws Exception {
-    if (no < 0 || no >= boardList.size()) {
-      return 0;
-    }
-    boardList.remove(no);
-    save();
-    return 1;
-  }
-
-  @Override
-  public void increaseViewCount(int no) throws Exception {
-    Board board = findByNo(no);
-    board.setViewCount(board.getViewCount() + 1);
-    save();
+    mapper.writeValue(new File(filename), boardList.toArray());
   }
 }
+
+
+
+
+
+
+
+
+
+
+
