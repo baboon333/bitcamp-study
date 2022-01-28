675d4d3c309321f63096141a356211a258758de6
supercode333 <gacomonkey123@gmail.com>
2022-01-28 18:07:10 +0900

    .

--- project-app1/app-client/src/main/java/com/eomcs/app1/ClientApp.java
+++ project-app1/app-client/src/main/java/com/eomcs/app1/ClientApp.java
@@ -9,17 +9,28 @@ public class ClientApp {
   public static void main(String[] args) throws Exception {
     System.out.println("[계산기 클라이언트]");
 
-    //1) 서버 애플리케이션과 네트워크 연결을 수행한다.
-    Socket socket = new Socket("192.168.0.249", 8888); // 서버와 연결될 때까지 객체를 생성하지 않는다.
+    Scanner keyScan = new Scanner(System.in);
+
+    System.out.println("요청(형식: 서버주소/연산자/값1/값2)? ");
+    String input = keyScan.nextLine();
+
+    int slashPos = input.indexOf("/"); // Pos: position
+    String serverAddress = input.substring(0, slashPos);
+    String queryString = input.substring(slashPos + 1); // slash 다음부터 끝까지 출력한다.
+
+    keyScan.close();
+
+
+    // 서버 애플리케이션과 네트워크 연결을 수행한다.
+    Socket socket = new Socket(serverAddress, 8888); // 서버와 연결될 때까지 객체를 생성하지 않는다.
     System.out.println("서버와 연결되었음!");
 
     // 데이터를 주고 받기 위한 입출력 스트림을 준비한다.
     PrintStream out = new PrintStream(socket.getOutputStream());
-
     Scanner in = new Scanner(socket.getInputStream());
 
     // 서버에 데이터를 보낸다.
-    out.println("상은");
+    out.println(input);
 
     // 서버가 응답한 데이터를 읽는다.
     String response = in.nextLine(); // 서버가 한 줄의 문자열을 보낼 때까지 리턴하지 않는다.
