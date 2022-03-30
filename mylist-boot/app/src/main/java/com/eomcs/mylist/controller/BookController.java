f7dc412f9fe6232f7b3baf831e8bff6ce260b406
supercode333 <gacomonkey123@gmail.com>
2022-03-31 02:22:45 +0900

    18.1 login

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BookController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BookController.java
@@ -1,4 +1,4 @@
-package com.eomcs.mylist.controller;
+/*package com.eomcs.mylist.controller;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -103,7 +103,8 @@ public class BookController {
           .body(resource); // 응답 콘텐트를 생성한 후 리턴한다.
 
     } catch (Exception e) {
-      e.printStackTrace();
+      //e.printStackTrace();
+      System.out.println("요청한 파일이 없습니다!");
       return null;
     }
   }
@@ -122,7 +123,7 @@ public class BookController {
 
       // 파일을 지정된 폴더에 저장한다.
       File photoFile = new File("./upload/book/" + filename); // App 클래스를 실행하는 프로젝트 폴더
-      file.transferTo(photoFile.getCanonicalFile()); // 프로젝트 폴더의 전체 경로(절대경로)를 전달한다. 상대 경로를 주니 file을 못찾아 에러가 난다.
+      file.transferTo(photoFile.getCanonicalFile()); // 프로젝트 폴더의 전체 경로를 전달한다.
 
       // 썸네일 이미지 파일 생성
       Thumbnails.of(photoFile)
@@ -140,3 +141,7 @@ public class BookController {
 
 
 }
+ */
+
+
+
